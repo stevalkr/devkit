50907656e15c4a0a92b651ff7cfcd3fddc1d1008
Steve Walker <zero.stevewalker@gmail.com>
2024-08-04 16:21:43 +0800

    update: flake

---
+++ flake/flake.nix
@@ -0,0 +1,67 @@
+rec {
+  description = "Devkit Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        apps.sk = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/sk";
+        };
+
+        packages.default = pkgs.callPackage
+          ({ lib, stdenv, lua, fmt, doctest, yaml-cpp, ninja, cmake, meson, pkg-config }:
+            stdenv.mkDerivation {
+              pname = "devkit";
+              version = "develop";
+
+              src = ../.;
+              dontUseCmakeConfigure = true;
+
+              buildInputs = [ lua fmt doctest yaml-cpp ];
+              nativeBuildInputs = [ ninja cmake meson pkg-config ];
+
+              meta = with lib; {
+                description = "devkit";
+                license = licenses.mit;
+              };
+            })
+          { };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.lua
+            pkgs.fmt
+            pkgs.doctest
+            pkgs.yaml-cpp
+          ];
+
+          nativeBuildInputs = [
+            pkgs.ninja
+            pkgs.cmake
+            pkgs.meson
+            pkgs.pkg-config
+            pkgs.ccache
+            pkgs.nixd
+            pkgs.nixpkgs-fmt
+            pkgs.clang-tools
+            pkgs.lua-language-server
+          ];
+
+          shellHook = ''
+            echo ${description}
+          '';
+        };
+      }
+    );
+}
